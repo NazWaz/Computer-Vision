cbd5471c7619d3b4bef02a2979d7ac518ae7647f
NazWaz <wazidur123@gmail.com>
2022-10-12 20:20:59 +0100

    adds get winner function

--- manual_rps.py
+++ manual_rps.py
@@ -1,20 +1,51 @@
 
 # %%
 import random # imports random module
+move_list = {"0": "Rock", "1": "Scissors", "2": "Paper"}
 
 def get_computer_choice():
-    move_list = {"0": "Rock", "1": "Scissors", "2": "Paper"}
-    computer_move = random.choice(list(move_list.values()))
+    computer_choice = random.choice(list(move_list.values()))
     print(move_list)
-    print(computer_move)
+    print(computer_choice)
 def get_user_choice():
-    user_move = input("Enter 'Rock', 'Paper' or 'Scissors'.")
-    print(user_move)
+    user_choice = input("Enter 'Rock', 'Paper' or 'Scissors'.")
+    print(user_choice)
 
 get_computer_choice()
 get_user_choice()
 
+def get_winner(computer_choice, user_choice):
+    if user_choice == move_list["0"] and computer_choice == move_list["1"]:
+        print("You have won!")
+    elif user_choice == move_list["1"] and computer_choice == move_list["2"]:
+        print("You have won!")
+    elif user_choice == move_list["2"] and computer_choice == move_list["0"]:
+        print("You have won!")
+
+get_winner()
+
     
 # %%
+def play():
 
 # %%
+import random # imports random module
+move_list = {"0": "Rock", "1": "Scissors", "2": "Paper"}
+computer_choice = "Paper"
+user_choice = "Paper"
+def get_winner(computer_choice, user_choice):
+    if (user_choice == move_list["0"] and computer_choice == move_list["1"] 
+    or user_choice == move_list["1"] and computer_choice == move_list["2"] 
+    or user_choice == move_list["2"] and computer_choice == move_list["0"]):
+        print("You have won!")
+    elif user_choice == computer_choice:
+        print("You have drawn!")
+    else:
+        print("You have lost!")
+    
+get_winner(computer_choice, user_choice)
+# %%
+elif user_choice == move_list["1"] and computer_choice == move_list["2"]:
+        print("You have won!")
+    elif user_choice == move_list["2"] and computer_choice == move_list["0"]:
+        print("You have won!")
