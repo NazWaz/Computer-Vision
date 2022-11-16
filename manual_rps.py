d1738d6014fea04bd7e92c85ceab8326bea5cccc
NazWaz <wazidur123@gmail.com>
2022-11-16 22:35:55 +0000

    adds docstrings

--- manual_rps.py
+++ manual_rps.py
@@ -1,37 +1,89 @@
 #%%
-import random # imports random module
+import random 
 
 class Game:
-    def __init__(self, move_list): # intialises parameters
+    '''
+    A Rock-Paper-Scissors game between the computer and user, played using a camera to detect the user's move.
+    The game is replayable and lasts until the user or computer reaches 3 wins.
+
+    Parameters:
+    ----------
+    move_list: dictionary
+        A list of possible moves in the game.
+
+    Attributes:
+    ----------
+    user_choice: str
+        The user's move.
+
+    Methods:
+    -------
+    get_computer_choice
+        Selects the move made by the computer.
+    get_user_choice
+        Asks the user for their move.
+    get_winner
+        Checks the result of the round between the computer and the user.
+    '''
+    def __init__(self, move_list):
 
         self.move_list = move_list # move list is given as an argument as a dictionary later
-        self.computer_choice = "" # computer choice is given an empty string value
         self.user_choice = ""   # user choice is given an empty string value
 
-    def get_computer_choice(self): # defines the computer choice function, passing only self as the argument
-        self.computer_choice = random.choice(list(self.move_list.values())) # chooses random values from a dictionary converted into a list
+    def get_computer_choice(self):
+        '''
+        Selects the move made by the computer randomly from a dictionary of moves:
+        - rock
+        - paper
+        - scissors
+        '''
+        computer_choice = random.choice(list(self.move_list.values())) 
 
-    def get_user_choice(self): # defines the user choice function, passing only self as the argument
-        while True: # makes the loop continuous
-            self.user_choice = input("Enter 'Rock', 'Paper' or 'Scissors'.") # asks for user's input
-            break # ends loop
+    def get_user_choice(self): 
+        '''
+        Asks the user to input their choice of move between:
+        - rock
+        - paper
+        - scissors
+        '''
+        while True: 
+            self.user_choice = input("Enter 'Rock', 'Paper' or 'Scissors'.") 
+            break 
     
-    def get_winner(self, computer_choice, user_choice): # defines the winner function, using the previous choice functions and self as arguments
-        if (user_choice == self.move_list["0"] and computer_choice == self.move_list["1"] 
-        or user_choice == self.move_list["1"] and computer_choice == self.move_list["2"] 
-        or user_choice == self.move_list["2"] and computer_choice == self.move_list["0"]): # condition where the user's choice beats the computer's choice
+    def get_winner(self, computer_choice):
+        '''
+        Checks the result of the round with 3 different checks:
+        1. If the user's move beats the computer's move
+        - this means the user wins the round and gets 1 point added to their round wins (user_wins)
+        2. If the user's move is identical to the computer's move
+        - this means the round is a draw
+        3. If the computer's move beats the user's move
+        - this means the computer wins the round and gets 1 point added to their round wins (computer_wins)
+        
+        Parameters:
+        ----------
+        computer_choice: str
+            The computer's move.
+        '''
+        if (self.user_choice == self.move_list["0"] and computer_choice == self.move_list["1"] 
+        or self.user_choice == self.move_list["1"] and computer_choice == self.move_list["2"] 
+        or self.user_choice == self.move_list["2"] and computer_choice == self.move_list["0"]): 
             print("You have won!")
-        elif user_choice == computer_choice: # condition where the user's choice is equal to the computer's choice
+        elif self.user_choice == computer_choice: 
             print("You have drawn!")
         else: 
             print("You have lost!")
 # %%
 def play():
-    game = Game(move_list = {"0": "Rock", "1": "Scissors", "2": "Paper"}) # creates the game instance using the Game class
+    '''
+    Plays the game by calling the get_computer_choice, get_user_choice and get_winner methods.
+    '''
+    game = Game({"0": "Rock", "1": "Scissors", "2": "Paper"}) 
     while True:
-        game.get_computer_choice() # calls the computer choice function
-        game.get_user_choice() # calls the user choice function
-        game.get_winner(game.computer_choice, game.user_choice) # calls the winner function, using the game instances' user and computer choices as arguments
+        game.get_computer_choice() 
+        game.get_user_choice() 
+        game.get_winner(game.computer_choice) 
         break
 # %%
 play()
+# %%
